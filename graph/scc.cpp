751dcaf7c67d7d7644cc877015bce8bae3535d78
kyonrevens <kyonrevens@gmail.com>
2017-11-09 14:24:19 +0700

    Create scc.cpp

---
+++ graph/scc.cpp
@@ -0,0 +1,24 @@
+vi S, visited;
+int numSCC;
+
+void tarjanSCC(int u) {
+  dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
+  S.push_back(u);
+  visited[u] = 1;
+  for (int j = 0; j < (int)AdjList[u].size(); j++) {
+    ii v = AdjList[u][j];
+    if (dfs_num[v.first] == DFS_WHITE)
+      tarjanSCC(v.first);
+    if (visited[v.first])
+      dfs_low[u] = min(dfs_low[u], dfs_low[v.first]);
+  }
+
+  if (dfs_low[u] == dfs_num[u]) {
+    printf("SCC %d:", ++numSCC);
+    while (1) {
+      int v = S.back(); S.pop_back(); visited[v] = 0;
+      printf(" %d", v);
+      if (u == v) break;
+    }
+    printf("\n");
+} }
